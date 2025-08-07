54658b8f491d8ec495b63ae3171f4f2932a8422e
Nadavgo8 <19nadav99@gmail.com>
2025-08-07 16:41:13 +0300

    completed ex1

--- src/components/Dummy.js
+++ src/components/Dummy.js
@@ -1,5 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-const Dummy = () => {}
+const Dummy = () => {
+  return (
+    <div>
+      <input type="text" />
+      <button>Submit</button>
+    </div>
+  );
+};
 
-export default Dummy
+export default Dummy;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Sum, App } from './App';
 import registerServiceWorker from './registerServiceWorker';
+require("http-parser-js").HTTPParser = global.HTTPParser =
+  require("http-parser-js").HTTPParser;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
