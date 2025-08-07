1b1063a41dccb2c1540e384fe9ee87e444756dfa
Nadavgo8 <19nadav99@gmail.com>
2025-08-07 16:47:23 +0300

    completed ex2

--- src/components/Spamalot.js
+++ src/components/Spamalot.js
@@ -2,7 +2,14 @@ import Spam from "./Spam";
 import React, { Component } from 'react';
 
 
-const Spamalot =()=>{}
+const Spamalot = () => {
+  const arr = [];
+  for (let i = 0; i < 500; i++) {
+    arr.push(<Spam />);
+  }
+  return arr;
+};
+
 
 export default Spamalot
 
